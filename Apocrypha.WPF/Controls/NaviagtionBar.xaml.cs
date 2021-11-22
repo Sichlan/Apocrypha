94fe815457b9aa46128ca9adae338f8af200623c
Sichlan IV <hyschommers@gmx.de>
2021-11-22 19:57:31 +0100

    Add Nav Bar

---
+++ Apocrypha.WPF/Controls/NaviagtionBar.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace Apocrypha.WPF.Controls
+{
+    public partial class NaviagtionBar : UserControl
+    {
+        public NaviagtionBar()
+        {
+            InitializeComponent();
+        }
+    }
+}
