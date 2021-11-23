521685f9b1e46ceea62246a719aab38f2896d970
Sichlan IV <hyschommers@gmx.de>
2021-11-23 22:46:22 +0100

    lel

---
+++ Apocrypha.WPF/Commands/LogoutCommand.cs
@@ -0,0 +1,18 @@
+﻿using Apocrypha.WPF.State.Navigators.Authenticators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Apocrypha.WPF.Commands
+{
+    public class LogoutCommand : AsyncCommandBase
+    {
+        private readonly IAuthenticator _authenticator
+        public override Task ExecuteAsync(object parameter)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
