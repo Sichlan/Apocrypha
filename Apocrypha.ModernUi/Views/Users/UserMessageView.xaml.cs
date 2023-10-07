dd66f43d6fb73640e6cdd5cf35723c44dd1bb62c
Vox <hyschommers@gmx.de>
2023-10-07 16:35:20 +0200

    Added content box to user message view

--- Apocrypha.ModernUi/Views/Users/UserMessageView.xaml.cs
+++ Apocrypha.ModernUi/Views/Users/UserMessageView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
 
 namespace Apocrypha.ModernUi.Views.Users;
 
@@ -7,5 +8,13 @@ public partial class UserMessageView : UserControl
     public UserMessageView()
     {
         InitializeComponent();
+    }
+
+    private void ButtonDetailsOnClick(object sender, RoutedEventArgs e)
+    {
+        if (TextMessageDetails.Visibility == Visibility.Collapsed)
+            TextMessageDetails.Visibility = Visibility.Visible;
+        else
+            TextMessageDetails.Visibility = Visibility.Collapsed;
     }
 }
